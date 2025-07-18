85337b35216a082bacaccd34130648114990192f
paul kiragu <paulkiragumburu@gmail.com>
2025-07-18 21:14:35 +0300

    cors configuration

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 // Initialize Socket.IO server
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "*" },
 });
 require("./socket")(io);
 
